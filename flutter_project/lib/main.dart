36cc0d7e3f0a44408841328b3d64df35927e1e04
desto12 <36659162+desto12@users.noreply.github.com>
2020-03-31 18:55:46 +0200

    Sign in page interface

--- flutter_project/lib/main.dart
+++ flutter_project/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Rezerwacja kortów',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Login Screen'),
     );
   }
 }
@@ -44,68 +46,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  TextStyle style = TextStyle(
+    color: Colors.grey,
+  );
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final emailField = TextField(
+      obscureText: false,
+      style:style,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0,15.0,20.0,15.0 ),
+        hintText: "E-mail",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    );
+    final password = TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.fromLTRB(20.0,15.0,20.0,15.0 ),
+        hintText: "Hasło",
+        border:
+        OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final loginButton = Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color:Color(0xfff50057),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(15.0, 20.0, 20.0, 15.0),
+          onPressed: (){},
+          child: Text("Login", textAlign: TextAlign.center),
+        )
+    );
+    final registerButton = Material(
+      child:SignInButtonBuilder(
+        text: 'Załóż konto',
+        icon: Icons.email,
+        onPressed: () {},
+        backgroundColor: Colors.blueGrey[700],
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(30.0)
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+    final fb_button = Material(
+      child:SignInButton(
+        Buttons.Facebook,
+        text: "Konto Facebook",
+        shape: RoundedRectangleBorder(
+          borderRadius: new BorderRadius.circular(30.0)
+          //side: BorderSide(color: Colors.black)
+        ),
+        onPressed: () {},
+      ),);
+    final google_button = SignInButton(
+      Buttons.Google,
+      text: "Konto Google",
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(30.0),
+      ),
+      onPressed: () {},
+    );
+
+
+    return Scaffold(
+        body: Center(
+            child: Container(
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 220,
+                        child: Image.asset(
+                            "assets/images/logo.png",
+                            fit: BoxFit.contain),
+                      ),
+                      SizedBox(
+                        height: 60.0,
+                        child: emailField,
+                      ),
+                      SizedBox(
+                        height: 70.0,
+                        child: password,
+                      ),
+                      loginButton,
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Column(children: [registerButton, google_button, fb_button ],mainAxisAlignment: MainAxisAlignment.center,),
+                    ],
+                  ),
+                )
+
+            )
+        )
     );
   }
 }
